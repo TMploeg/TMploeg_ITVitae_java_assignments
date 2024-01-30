4c95993d94bdd2b5c9dbf2d07ad464a6c384cb24
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-01-30 15:38:18 +0100

    finished VelocityCalculator

--- week_2/Week2Main.java
+++ week_2/Week2Main.java
@@ -3,7 +3,8 @@ public class Week2Main{
 		String[] options = {
 			"calculate minutes",
 			"convert Celcius to Fahrenheit",
-			"convert Fahrenheit to Celcius"
+			"convert Fahrenheit to Celcius",
+			"calculate velocity"
 		};
 		
 		for(int i = 0; i < options.length; i++){
@@ -14,14 +15,7 @@ public class Week2Main{
 		
 		switch(selectedOption){
 			case 1:
-				int years = ConsoleHelper.getPositiveNumberInput("Nr. or years:");
-				int months = ConsoleHelper.getPositiveNumberInput("Nr. or months:");
-				int days = ConsoleHelper.getPositiveNumberInput("Nr. or days:");
-				int hours = ConsoleHelper.getPositiveNumberInput("Nr. or hours:");
-				
-				long nrOfMinutes = TimeConverter.calculateMinutes(years,months,days,hours);
-				
-				System.out.println("minutes: " + nrOfMinutes);
+				runConvertMinutes();
 				break;
 			case 2:
 				double celcius = ConsoleHelper.getDoubleInput("temperature in Celcius");
@@ -31,14 +25,37 @@ public class Week2Main{
 				double fahrenheit = ConsoleHelper.getDoubleInput("temperature in Fahrenheit");
 				System.out.println("temperature in Celcius: " + TemperatureConverter.convertFahrenheitToCelcius(fahrenheit));
 				break;
+			case 4:
+				runCalculateVelocity();
+				break;
+			case 5:
+				runCaesarEncode();
+				break;
 			default: throw new RuntimeException("should not reach");
 		}
 	}
 	
-	/*
-	int years = getPositiveNumberInput("Nr. or years:");
-		int months = getPositiveNumberInput("Nr. or months:");
-		int days = getPositiveNumberInput("Nr. or days:");
-		int hours = getPositiveNumberInput("Nr. or hours:");
-	*/
+	private static void runConvertMinutes(){
+		int years = ConsoleHelper.getPositiveNumberInput("Nr. or years:");
+		int months = ConsoleHelper.getPositiveNumberInput("Nr. or months:");
+		int days = ConsoleHelper.getPositiveNumberInput("Nr. or days:");
+		int hours = ConsoleHelper.getPositiveNumberInput("Nr. or hours:");
+		
+		long nrOfMinutes = TimeConverter.calculateMinutes(years,months,days,hours);
+		
+		System.out.println("minutes: " + nrOfMinutes);
+	}
+	
+	private static void runCalculateVelocity(){
+		int meters = ConsoleHelper.getPositiveNumberInput("nr of meters");
+		int hours = ConsoleHelper.getPositiveNumberInput("nr of hours");
+		int minutes = ConsoleHelper.getPositiveNumberInput("nr of minutes");
+		int seconds = ConsoleHelper.getPositiveNumberInput("nr of seconds");
+		
+		VelocityCalculator.VelocityData vData = VelocityCalculator.calculate(meters, new VelocityCalculator.TimeData(hours, minutes, seconds));
+		
+		System.out.println("speed in meters/second: " + vData.metersPerSec());
+		System.out.println("speed in km/hour: " + vData.kmPerHour());
+		System.out.println("speed in miles/hour: " + vData.milesPerHour());
+	}
 }
