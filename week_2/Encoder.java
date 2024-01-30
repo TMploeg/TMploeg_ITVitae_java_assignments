896eeba639b7069db1fbfabae93ef4111b36271e
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-01-30 15:49:44 +0100

    Update Encoder.java

--- week_2/Encoder.java
+++ week_2/Encoder.java
@@ -2,10 +2,14 @@ public class Encoder{
 	private static final int CAESAR_OFFSET = 3;
 	
 	public static String caesarEncode(String str){
+		if(str == null){
+			throw new NullPointerException("'str' is null");
+		}
+		
 		String encodedStr = "";
 		
-		for(char c : str.toCharArray()){
-			encodedStr += (char)(c + CAESAR_OFFSET);
+		for(int i = 0; i < str.length(); i++){
+			encodedStr += (char)(str[i] + CAESAR_OFFSET);
 		}
 		
 		return encodedStr;
